16c701bfe9fdd93d57a198a1534ed9a978283873
milimiliemilie <66954866+milimiliemilie@users.noreply.github.com>
2020-08-17 20:21:31 +0900

    Project Start

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
+app = Flask(__name__)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
