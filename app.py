47fc8ef3c1ca45dfedc6896d79fa7f26c7609fe1
milimiliemilie <66954866+milimiliemilie@users.noreply.github.com>
2020-08-19 21:42:43 +0900

    index만 겨우 손봄

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, jsonify, request
+import requests
 from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+from bs4 import BeautifulSoup
+import re, demjson
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
@@ -13,3 +16,21 @@ def home():
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.moel.go.kr/pension/finance/rate2.do#this',headers=headers)
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+soup = BeautifulSoup(data.text, 'html.parser')
+#############################
+
+# copyselector : #jqg214
+rateRecords = soup.select('tr')
+
+# ('meta[property="og:title"]')
+
+for rateRecord in rateRecords:
+    td_tag = rateRecord.select_one('td[property="jqGrid_rate"]')
+
+print(td_tag.text)
