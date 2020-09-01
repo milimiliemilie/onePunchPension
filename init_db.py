1c61c645f6ab751e8bd0b97b8239e55b9c2d63ba
milimiliemilie <66954866+milimiliemilie@users.noreply.github.com>
2020-09-01 20:13:26 +0900

    float 0.00 으로 나오게 하는데 성공

--- init_db.py
+++ init_db.py
@@ -274,25 +274,57 @@ listSet = [
 # - DB : db.sparta.penrate
 # - doc 에 저장해야 하는딩 ... print 한방에 하는 것처럼 ...
 
+# 숫자로 만드는 게 또 관건이네 ...
+
+def two_decimals(num):
+    return (format(num, ".2f"))
+
+
 def to_float(rawRate):
     try:
-        return float(rawRate)
+        return two_decimals(float(rawRate))
     except ValueError:
-        return 0
+        return two_decimals(0)
+
+
+def get_html():
+    for srchYear in [2020]:
+        for srchMonth in [8]:
+            payload = 'srchMonth=' + str(srchMonth) + '&srchWord=&srchYear=' + str(srchYear) + '&url=on'
+
+            headers = {
+                'Upgrade-Insecure-Requests': '1',
+                'Origin': 'https://www.moel.go.kr',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+            }
+            response = requests.request("POST", url, headers=headers, data=payload)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            p = re.compile(r'var mydata = (.*?);', re.DOTALL)
+            script = soup.select_one('script[type="text/javascript"]:last-child')
+            m = p.findall(response.text)
+
+            data = demjson.decode(m[0])
+
+            pprint.pprint(data)
 
 
 # print(to_float('1.1'))
 # print(to_float(''))
 
 def insert_rate(data):
+    # 위에서 정한 {'회사명', '금리연동형 상품명', 'DB 상품명', 'DC'상품명'}이 있는지 하나하나 돌아가면서 찾는다
     for i in range(0, len(listSet)):
+        # data = URL에서 퍼온 크롤링 대상 표
+        # d    = 표에서 따온 한줄한줄 {
         for d in data:
             if d['company'] == listSet[i]['companySet']:
                 if listSet[i]['product_floatRate'] != "":
                     if d['product'] == listSet[i]['product_floatRate']:
-                        floatRate = float(d['rate'])
+                        floatRate = two_decimals(float(d['rate']))
                 else:
-                    floatRate = 0
+                    floatRate = two_decimals(0)
                 if d['product'] == listSet[i]['product_db']:
                     db1y = to_float(d['val12'])
                     db2y = to_float(d['val24'])
@@ -332,48 +364,136 @@ def insert_rate(data):
             'srchYear': srchYear,
             'srchMonth': srchMonth
         }
-        # db.penrate.insert_one(doc)
-        print(doc)
+        db.penrate.insert_one(doc)
+        # pprint.pprint(doc)
+
 
 # 이 아래로 크롤링
 
-url = "https://www.moel.go.kr/pension/finance/rate2.do"
 
+url = "https://www.moel.go.kr/pension/finance/rate2.do"
 years = list(range(2017, 2020))  # 2017 ~ 2019
 months = list(range(1, 13))  # 1 ~ 12
-
-# for srchYear in years:
-#     for srchMonth in months:
-#         print(srchYear, srchMonth)
 
 
 # 2016. 2 ~ 12 월 : 별도 수행
 # 2020. 1 ~ 8 월 : 별도 수행
 
-for srchYear in [2020]:
-    for srchMonth in list(range(1, 9)):
-        payload = 'srchMonth=' + str(srchMonth) + '&srchWord=&srchYear=' + str(srchYear) + '&url=on'
+# 0.00 으로 형식이 다 나오는지 보자. -> 된다.
 
-        headers = {
-            'Upgrade-Insecure-Requests': '1',
-            'Origin': 'https://www.moel.go.kr',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-        }
-        response = requests.request("POST", url, headers=headers, data=payload)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        p = re.compile(r'var mydata = (.*?);', re.DOTALL)
-        script = soup.select_one('script[type="text/javascript"]:last-child')
-        m = p.findall(response.text)
+def create_document():
+    for srchYear in [2020]:
+        for srchMonth in list(range(1, 9)):
+            payload = 'srchMonth=' + str(srchMonth) + '&srchWord=&srchYear=' + str(srchYear) + '&url=on'
 
-        data = demjson.decode(m[0])
+            headers = {
+                'Upgrade-Insecure-Requests': '1',
+                'Origin': 'https://www.moel.go.kr',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+            }
+            response = requests.request("POST", url, headers=headers, data=payload)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            p = re.compile(r'var mydata = (.*?);', re.DOTALL)
+            script = soup.select_one('script[type="text/javascript"]:last-child')
+            m = p.findall(response.text)
 
-        insert_rate(data)
+            data = demjson.decode(m[0])
 
-        print(str(srchYear) + '.' + str(srchMonth) + '. 완료')
+            insert_rate(data)
+
+            print(str(srchYear) + '.' + str(srchMonth) + '. 완료')
 
 
+# 전월비를 만들어 줄 거야.
+# 먼저 ... 2019.5 -> 2019.6 전월비 of 현대라이프 1년형 으로 해봐야겟다 (2.45 -> 2.52, +#)
 
-# 숫자로 만드는 게 또 관건이네 ...
+def test_change():
+    # 2019.5 현대라이프 1년형 : { 'coName': '현대라이프', 'db1y': 2.45, 'srchMonth': 5, 'srchYear': 2019 }
+    this_db1y_2019_5 = db.penrate.find_one({'coName': '현대라이프', 'srchMonth': 5, 'srchYear': 2019})['db1y']
+    print(this_db1y_2019_5)
 
+    # 2019.6 현대라이프 1년형 : { 'coName': '현대라이프', 'db1y': 2.52, 'srchMonth': 5, 'srchYear': 2019 }
+    this_db1y_2019_6 = db.penrate.find_one({'coName': '현대라이프', 'srchMonth': 6, 'srchYear': 2019})['db1y']
+    print(this_db1y_2019_6)
+
+    # 2019.5 -> 2019.6 현대라이프 1년형 전월비
+    # 소수점 두자리까지만 나오게 해야겠다
+    this_db1y_2019_6_change = this_db1y_2019_6 - this_db1y_2019_5
+    if this_db1y_2019_6_change < 0:
+        rateChange = "-"
+    else:
+        rateChange = "+"
+
+    print(rateChange + str(round(this_db1y_2019_6_change * 100)) + 'bp')
+
+    # coName, srchMonth, srchYear를 돌아가면서,
+    # 각 상품들 (db1y, db2y, ...) 에 대해 함수를 짜준다
+
+
+# get_html(): 그냥 테스트용으로 html 끌어온 것.
+# 실제로 쓸 코드는 저 위에 있는 create_document() 이다.
+
+
+### floatRate 이 0인 document 들은 -> floatRate field 를 two_decimals(0) 으로 Update 했다 :
+
+# db.penrate.update_many({'floatRate': 0}, {'$set': {'floatRate': two_decimals(0)}})
+# print(list(db.penrate.find({'floatRate': 0}, {'_id': 0})))
+
+
+# coName 을 listSet에서 빼오자
+# listSet: 29개
+
+coNames = []
+cs = list(range(0, 29))
+for c in cs:
+    tempCoName = listSet[c]['companySet']
+    coNames.append(tempCoName)
+# 결과 : coNames = ['BNK경남은행', 'DB생명', 'DB손보', ... ]
+print(coNames)
+
+pprint.pprint(list(db.penrate.find({'srchMonth': 2, 'srchYear': 2016})))
+
+def add_rate_change():
+
+    products = ['floatRate', 'db1y', 'db2y', 'db3y', 'db5y', 'dc1y', 'dc2y', 'dc3y', 'dc5y']
+
+    # find: 각 상품별로 가져온다. 지금꺼랑, 전월꺼를.
+    # update: (지금꺼 - 전월꺼) value 를 집어넣는다.
+
+    for srchYear in [2016]:
+        for srchMonth in [4, 13]:
+            for coName in coNames:
+                for product in products:
+
+                    lastRate = float(db.penrate.find_one({'coName': coName, 'srchMonth': srchMonth-1, 'srchYear': srchYear})[product])
+                    nowRate = float(db.penrate.find_one({'coName': coName, 'srchMonth': srchMonth, 'srchYear': srchYear})[product])
+
+                    rateChange = nowRate - lastRate
+                    if rateChange < 0:
+                        changeSign = ""
+                    else:
+                        changeSign = "+"
+
+                    print(changeSign + str(round(rateChange * 100)) + 'bp')
+
+
+# def add_rate_chage():
+# company = [], srchYear = [], ... : 돌아갈 순번들을 정하고 (회사명, 상품명, year, month)
+# for ... in [] ... : 돌아가게 만든다
+# rateChange 값을 Update 시킨다
+# 단, 필드 값을 Update 할 때... 과거값이 없으면 to_float(0) 으로 둔다.
+
+# 1. create_document() 돌려서, 0.00 형태로 다 바꿔주고
+# 2. add_rate_change() 를 짜고,
+# 3. add_rate_chagne() 를 돌려서, +/-##bp 를 채워준다
+
+# 4. index.html 에서 rateChange 를 불러올 수 있게 한다.
+
+### Executions:
+
+# create_document()
+# test_change()
+# get_html()
+# add_rate_change()
